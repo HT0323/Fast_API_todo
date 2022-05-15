ebf84fa69a6641d394c3962eceb8900d81bb9415
HaraT0323 <alra9si916323@gmail.com>
2022-05-15 21:55:34 +0900

    タスク取得機能の追加

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from decouple import config
 from typing import Union
 import motor.motor_asyncio
+from bson import ObjectId
 
 MONGO_API_KEY = config('MONGO_API_KEY')
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_API_KEY)
@@ -23,4 +24,18 @@ async def db_create_todo(data: dict) -> Union[dict, bool]:
     new_todo = await collection_todo.find_one({"_id": todo.inserted_id})
     if new_todo:
         return todo_serializer(new_todo)
+    return False
+
+
+async def db_get_todos() -> list:
+    todos = []
+    for todo in await collection_todo.find().to_list(length=100):
+        todos.append(todo_serializer(todo))
+    return todos
+
+
+async def db_get_single_todo(id: str) -> Union[dict, bool]:
+    todo = await collection_todo.find_one({"_id": ObjectId(id)})
+    if todo:
+        return todo_serializer(todo)
     return False
--- routers/route_todo.py
+++ routers/route_todo.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter
 from fastapi import Response, Request, HTTPException
 from fastapi.encoders import jsonable_encoder
 from schemas import Todo, TodoBody
-from database import db_create_todo
+from database import db_create_todo, db_get_todos, db_get_single_todo
 from starlette.status import HTTP_201_CREATED
+from typing import List
 
 router = APIRouter()
 
@@ -17,4 +18,20 @@ async def create_todo(reequest: Request, response: Response, data: TodoBody):
         return res
     raise HTTPException(
         status_code=404, detail="Create task failed"
+    )
+
+
+@router.get("/api/todo", response_model=List[Todo])
+async def get_todos():
+    res = await db_get_todos()
+    return res
+
+
+@router.get("/api/todo/{id}", response_model=Todo)
+async def get_single_todo(id: str):
+    res = await db_get_single_todo(id)
+    if res:
+        return res
+    raise HTTPException(
+        status_code=404, detail=f"Task of ID:{id} doesn't exist"
     )
